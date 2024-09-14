556735ae43056312400b5932f1074cf6e86e9ee4
TeeZeSheng <109713398+TeeZeSheng@users.noreply.github.com>
2024-09-14 14:09:44 +0800

    Update userRoute.js

--- route/userRoute.js
+++ route/userRoute.js
@@ -11,29 +11,29 @@ router.post('/login', authController.signin);
 
 // router.get('/me', authController.protect, userController.getOneUser)
 
-router.post('/forgotPassword', authController.forgetPassword);
+// router.post('/forgotPassword', authController.forgetPassword);
 
-router.patch('/resetPassword/:token', authController.resetPassword);
+// router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch('/changePassword', authController.protect, authController.updatePassword);
+// router.patch('/changePassword', authController.protect, authController.updatePassword);
 
-router.patch('/updateData', authController.protect, userController.uploadPhoto, userController.updateMe);
+// router.patch('/updateData', authController.protect, userController.uploadPhoto, userController.updateMe);
 
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
+// router.route('/').get(userController.getAllUsers).post(userController.createUser);
 
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
+// router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
-router.route('/preference').post(authController.protect, userController.setPreference);
+// router.route('/preference').post(authController.protect, userController.setPreference);
 
-router.route('/userLoginDets').get(authController.getLoginDetails)
+// router.route('/userLoginDets').get(authController.getLoginDetails)
 
-router.route('/updateProgress').post(authController.protect, userController.setProgress)
+// router.route('/updateProgress').post(authController.protect, userController.setProgress)
 
-router.route('/getProgress/:userId').get(authController.protect, userController.getProgress)
+// router.route('/getProgress/:userId').get(authController.protect, userController.getProgress)
 
-router.get('/getUser/:id', authController.protect, userController.getOneUser)
+// router.get('/getUser/:id', authController.protect, userController.getOneUser)
 
 
 
