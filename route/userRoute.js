1dd269738ebf8c11ac177ec57289281af97460fc
TeeZeSheng <109713398+TeeZeSheng@users.noreply.github.com>
2024-09-14 13:20:29 +0800

    Update userRoute.js

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
-const workoutController = require('./../controllers/workoutController');
-const mealPlanController = require('./../controllers/mealPlanController')
+
 
 const router = express.Router();
 
